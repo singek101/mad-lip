569b53c2d6c190ff828dd7daab04e870ba4f1ef3
singek101 <michaelmusediku@gmail.com>
2020-06-13 05:49:05 +0100

    mad lip game

---
+++ app.py
@@ -0,0 +1,9 @@
+name = input("Enter a name: ")
+animal = input("Enter animal name: ")
+place = input("Enter a place: ")
+color = input("Enter a color: ")
+noun = input("Enter a noun that has the color stated: ")
+
+poem = f"{name} had a little {animal}\nIt was {color} as {noun}\nEverywhere {name} went\nThe {animal} was sure to go\nIt followed {name} to {place} one day\nAnd broke the {place} rule\nAnd what a time did they have\nThat day at {place}."
+
+print(poem)
